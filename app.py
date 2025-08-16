13e1653cf54c2dac74020b0866dcaa9f19667ed3
Christian Zavala <crizaaron@gmail.com>
2025-08-16 17:54:12 -0600

    Update

--- app.py
+++ app.py
@@ -18,40 +18,32 @@ def whatsapp_webhook():
     estado = usuarios_estado.get(user_id, "Inicio")
 
     resp = MessagingResponse()
-    msg = resp.message()
 
     if estado == "Inicio":
-        msg.body(menu_bienvenida + "Estado: " + estado)
+        msg = resp.message()
+        msg.body(menu_bienvenida)
         usuarios_estado[user_id] = "esperando_en_menu"
 
     elif estado == "esperando_en_menu":
-        mensaje, nuevo_estado = respuesta_menu_precio(incoming_msg) 
-        msg.body(mensaje + "Estado: " + estado)
+        mensaje, nuevo_estado = respuesta_menu_precio(incoming_msg)
+        msg = resp.message()
+        msg.body(mensaje)
         usuarios_estado[user_id] = nuevo_estado
-        
+
     elif estado == "menu_precios":
         mensaje, nuevo_estado = respuesta_confirmacion(incoming_msg)
-        msg.body(mensaje + "Estado: " + estado)
+        msg = resp.message()
+        msg.body(mensaje)
         usuarios_estado[user_id] = nuevo_estado
 
-    # elif estado == "menu_clase":
-    #     mensaje, nuevo_estado = (None, "Inicio")  # Aquí deberías definir la lógica para el menú de clases
-    #     msg.body(mensaje)
-    #     usuarios_estado[user_id] = nuevo_estado
-
-    # elif estado == "menu_inscripcion":
-    #     mensaje, nuevo_estado = (None, "Inicio")  # Aquí deberías definir la lógica para el menú de clases
-    #     msg.body(mensaje)
-    #     usuarios_estado[user_id] = nuevo_estado
-    
     else:
-        msg.body("Reiniciando Estado" + "Estado: " + estado)
+        msg = resp.message()
+        msg.body("Reiniciando Estado")
         usuarios_estado[user_id] = "Inicio"
         msg2 = resp.message()
-        msg2.body(menu_bienvenida + "Estado: " + usuarios_estado[user_id])
+        msg2.body(menu_bienvenida)
 
     return Response(str(resp), mimetype="application/xml", status=200)
-
 if __name__ == "__main__":
     app.run(debug=True)
 
