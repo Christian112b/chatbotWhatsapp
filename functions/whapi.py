597bed1775964a4ec4898f797b8e41e69ae73151
Christian Zavala <crizaaron@gmail.com>
2025-08-17 23:25:48 -0600

    Update

---
+++ functions/whapi.py
@@ -0,0 +1,17 @@
+import requests
+import os
+
+def send_whapi_message(to, message):
+    url = "https://gate.whapi.cloud/send-message"
+    headers = {
+        "Authorization": f"Bearer {os.environ.get('WHAPI_TOKEN')}",
+        "Content-Type": "application/json"
+    }
+    payload = {
+        "to": to.replace("whatsapp:", "").replace("+", ""),
+        "text": message
+    }
+
+    response = requests.post(url, headers=headers, json=payload)
+    
+    return response.status_code, response.text
